232c6f52748e728f965e5db00cf7130a60ce7a73
Neil Stoker <neil.stoker@sgpconsulting.co.uk>
2017-04-24 13:21:39 +0100

    Some perparation for the next bit

---
+++ Scripts/client_sm.lua
@@ -0,0 +1,19 @@
+--- Statemachine definition
+--  For testing the statemachine callbacks
+
+local machine = require('lua-state-machine/statemachine')﻿
+
+function sm_config ()
+  {
+    local fsm = machine.create({
+      intial = 'green',
+      events = {
+        { name = 'warn', from = 'green', to = 'yellow'},
+        { name = 'panic', from = 'yellow', to = 'red'},
+        { name = 'calm', from = 'red', to = 'yellow'},
+        { name = 'clear', from = 'yellow', to = 'green'},
+      }
+    })
+
+    return fsm
+}
---
+++ Scripts/smwrapper.lua
@@ -0,0 +1,14 @@
+--- Testscript wrapper for running the state machine
+-- To reproduce the calls made from the main system
+
+--require 'Scripts/client_sm.lua'
+
+--sm = client_sm:sm_config()
+--sm:todot('sm.dot')
+
+function getStates()
+  {
+    return "oops" --sm.events;
+}
+
+return 'oops'
