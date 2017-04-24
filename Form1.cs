b1a9cd8d1e9ad1ca38738652d8ee89584e334059
Neil Stoker <neil.stoker@sgpconsulting.co.uk>
2017-04-24 10:37:10 +0100

    Tutorial: Getting Started GREEN

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MoonSharp.Interpreter;
+using MoonSharp.Interpreter.Loaders;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,54 @@ namespace shiney_waffle
 {
     public partial class Form1 : Form
     {
+        private Script luaScript;
+
         public Form1()
         {
             InitializeComponent();
+
+            InitialiseMoonsharp();
+        }
+
+        private string MoonSharpFactorial()
+        {
+            string script = @"
+                -- defines a factorial function
+                function fact(n)
+                  if(n==0) then
+                    return 1
+                  else
+                    return n*fact(n-1)
+                  end
+                end
+
+                return fact(5)";
+            return script;
+        }
+
+        private void InitialiseMoonsharp()
+        {
+            //// Sets up the moonsharp environment
+            //try
+            //{
+            //    luaScript = new Script();
+            //    luaScript.Options.ScriptLoader = new EmbeddedResourcesScriptLoader();
+            //    Script.DefaultOptions.ScriptLoader = new EmbeddedResourcesScriptLoader();
+            //    ((ScriptLoaderBase)luaScript.Options.ScriptLoader).ModulePaths = new string[] { "Scripts/?.lua", "Scripts/?" };
+            //    luaScript.LoadFile("Scripts/smwrapper.lua",null,"wrapper");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"EXCEPTION {ex}");
+            //    throw;
+            //}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //DynValue res = luaScript.Globals.Get(luaScript.Globals["getStates"]);
+            DynValue res = Script.RunString(MoonSharpFactorial());
+            label1.Text = "Result of function is " + res.ToString();
         }
     }
 }
