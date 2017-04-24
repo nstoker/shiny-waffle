eafa31d922638b3a0b308df81fdbf414fd21f07d
Neil Stoker <neil.stoker@sgpconsulting.co.uk>
2017-04-24 11:03:02 +0100

    Returning a sequence of numbers

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,7 @@
 ﻿using MoonSharp.Interpreter;
 using MoonSharp.Interpreter.Loaders;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,7 +15,7 @@ namespace shiney_waffle
 {
     public partial class Form1 : Form
     {
-        private Script luaScript;
+        private static Script script;
 
         public Form1()
         {
@@ -47,13 +48,41 @@ namespace shiney_waffle
         {
             string scriptCode = MoonSharpFactorialSource();
 
-            luaScript = new Script();
+            script = new Script();
 
-            luaScript.DoString(MoonSharpFactorialSource());
+            script.DoString(MoonSharpFactorialSource());
 
-            luaScript.Globals["Mul"] = (Func<int,int,int>)Mul;
-            DynValue res = luaScript.Call(luaScript.Globals["fact"], 4);
+            script.Globals["Mul"] = (Func<int,int,int>)Mul;
+            DynValue res = script.Call(script.Globals["fact"], 4);
             return res.Number;
+        }
+
+        private static IEnumerable<int> GetNumbers()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                yield return i;
+            }
+        }
+
+        private static double EnumerableTest()
+        {
+            string scriptCode = @"
+                total = 0;
+
+                for i in getNumbers() do
+                  total = total + i;
+                end
+
+                return total;
+                ";
+
+            script = new Script();
+            script.Globals["getNumbers"] = (Func<IEnumerable<int>>)GetNumbers;
+            
+            DynValue res = script.DoString(scriptCode);
+            return res.Number;
+            
         }
         private void InitialiseMoonsharp()
         {
@@ -76,7 +105,7 @@ namespace shiney_waffle
         private void button1_Click(object sender, EventArgs e)
         {
             //DynValue res = luaScript.Globals.Get(luaScript.Globals["getStates"]);
-            label1.Text = "Result of function is " + CallbackTest().ToString();
+            label1.Text = "Result of function is " + EnumerableTest().ToString();
         }
     }
 }
